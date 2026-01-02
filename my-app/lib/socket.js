446bde5f55ced7a803b751584ae409dd025a1394
KrPrince19 <princekumarydv19@gmail.com>
2026-01-02 21:37:26 +0530

    Update socket.js

--- my-app/lib/socket.js
+++ my-app/lib/socket.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 export const socket = io(
-  process.env.NEXT_PUBLIC_SOCKET_URL || "http://localhost:5000",
+  process.env.NEXT_PUBLIC_SOCKET_URL || "https://bgmibackend-uc71.vercel.app/",
   {
     transports: ["websocket"],
   }
