47911d94f873fdeaecabe3350cac6fd37ca22ed3
KrPrince19 <princekumarydv19@gmail.com>
2026-01-02 16:10:19 +0530

    Update socket.js

--- my-app/lib/socket.js
+++ my-app/lib/socket.js
@@ -1,7 +1,9 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000", {
-  transports: ["websocket"],
-});
-
-export default socket;
+export const socket = io(
+  process.env.NEXT_PUBLIC_SOCKET_URL || "http://localhost:5000",
+  {
+    transports: ["websocket"],
+    autoConnect: true,
+  }
+);
