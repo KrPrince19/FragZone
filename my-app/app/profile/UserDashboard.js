395cdc9b9ddb06da4085dfb88b69b073241f28ba
KrPrince19 <princekumarydv19@gmail.com>
2026-01-03 21:58:57 +0530

    Update UserDashboard.js

--- my-app/app/profile/UserDashboard.js
+++ my-app/app/profile/UserDashboard.js
@@ -14,7 +14,7 @@ export default function UserDashboard({ userEmail, name }) {
   /* ================= FETCH JOINED MATCHES ================= */
   const fetchTournamentDetails = async () => {
     try {
-      const res = await fetch("https://bgmibackend.onrender.com/joinmatches");
+      const res = await fetch("https://bgmibackend.vercel.app/joinmatches");
       if (!res.ok) throw new Error(`Server responded with ${res.status}`);
       const data = await res.json();
       setJoindata(data);
