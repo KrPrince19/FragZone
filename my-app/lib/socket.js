3030ff6339a31e6a273cfe340d0dc4960f13ef77
KrPrince19 <princekumarydv19@gmail.com>
2026-01-06 23:08:02 +0530

    Update socket.js

--- my-app/lib/socket.js
+++ my-app/lib/socket.js
@@ -1,8 +1,9 @@
 import { io } from "socket.io-client";
 
-export const socket = io(
-  process.env.NEXT_PUBLIC_SOCKET_URL,
-  {
-    transports: ["websocket"],
-  }
-);
+export const socket = io(process.env.NEXT_PUBLIC_SOCKET_URL, {
+  // Polling handles the initial "wake up" of Render servers better
+  transports: ["polling", "websocket"],
+  reconnection: true,
+  reconnectionAttempts: Infinity,
+  reconnectionDelay: 2000,
+});
